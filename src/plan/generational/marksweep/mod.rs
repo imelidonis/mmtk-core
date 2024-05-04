58235112e9caab19d03da9cdc8037e234e4768ac
Ioannis Melidonis <imel@csd.uoc.gr>
2024-05-04 17:56:45 +0300

    GenMS Allocator

--- src/plan/generational/marksweep/mod.rs
+++ src/plan/generational/marksweep/mod.rs
@@ -1,4 +1,7 @@
 //! Plan: generational marksweep
 
 pub(in crate::plan) mod global;
+pub(in crate::plan) mod mutator;
+
+pub use self::global::GenMarkSweep;
 
---
+++ src/plan/generational/marksweep/mutator.rs
@@ -0,0 +1,53 @@
+pub(super) use super::super::ALLOCATOR_MAPPING;
+use super::GenMarkSweep;
+use crate::plan::barriers::ObjectBarrier;
+use crate::plan::generational::barrier::GenObjectBarrierSemantics;
+use crate::plan::generational::create_gen_space_mapping;
+use crate::plan::mutator_context::unreachable_prepare_func;
+use crate::plan::mutator_context::Mutator;
+use crate::plan::mutator_context::MutatorConfig;
+use crate::plan::AllocationSemantics;
+use crate::util::alloc::allocators::Allocators;
+use crate::util::alloc::BumpAllocator;
+use crate::util::{VMMutatorThread, VMWorkerThread};
+use crate::vm::VMBinding;
+use crate::MMTK;
+
+pub fn genmarksweep_mutator_release<VM: VMBinding>(mutator: &mut Mutator<VM>, _tls: VMWorkerThread) {
+    // reset nursery allocator
+    let bump_allocator = unsafe {
+        mutator
+            .allocators
+            .get_allocator_mut(mutator.config.allocator_mapping[AllocationSemantics::Default])
+    }
+    .downcast_mut::<BumpAllocator<VM>>()
+    .unwrap();
+    bump_allocator.reset();
+}
+
+pub fn create_genmarksweep_mutator<VM: VMBinding>(
+    mutator_tls: VMMutatorThread,
+    mmtk: &'static MMTK<VM>,
+) -> Mutator<VM> {
+    let genmarksweep = mmtk.get_plan().downcast_ref::<GenMarkSweep<VM>>().unwrap();
+    let config = MutatorConfig {
+        allocator_mapping: &ALLOCATOR_MAPPING,
+        space_mapping: Box::new(create_gen_space_mapping(
+            mmtk.get_plan(),
+            &genmarksweep.gen.nursery,
+        )),
+        prepare_func: &unreachable_prepare_func,
+        release_func: &genmarksweep_mutator_release,
+    };
+
+    Mutator {
+        allocators: Allocators::<VM>::new(mutator_tls, mmtk, &config.space_mapping),
+        barrier: Box::new(ObjectBarrier::new(GenObjectBarrierSemantics::new(
+            mmtk, genmarksweep,
+        ))),
+        mutator_tls,
+        config,
+        plan: genmarksweep,
+    }
+}
+
